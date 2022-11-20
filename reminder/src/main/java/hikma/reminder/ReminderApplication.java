bf18c94d120e687965300f9885ce3c67d6a87fcf
nkrumah <79527821+vsvincent@users.noreply.github.com>
2022-11-20 14:49:23 +0100

    Create project

---
+++ reminder/src/main/java/hikma/reminder/ReminderApplication.java
@@ -0,0 +1,12 @@
+package hikma.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderApplication.class, args);
+	}
+}
