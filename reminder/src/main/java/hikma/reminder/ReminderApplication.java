a6391a2eb94195a91507d8f6d420ee70076d2215
nkrumah <79527821+vsvincent@users.noreply.github.com>
2022-12-06 14:36:25 +0100

    Update main class to launch AWT menu

--- reminder/src/main/java/hikma/reminder/ReminderApplication.java
+++ reminder/src/main/java/hikma/reminder/ReminderApplication.java
@@ -1,7 +1,10 @@
 package hikma.reminder;
 
+import hikma.reminder.api.gui.MainMenu;
+
 public class ReminderApplication {
 
 	public static void main(String[] args) {
+		MainMenu mainMenu = new MainMenu();
 	}
 }
