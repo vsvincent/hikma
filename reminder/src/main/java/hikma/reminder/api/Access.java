bb4d406b23b505ba8ead524b3fbd8a3d802853b7
nkrumah <79527821+vsvincent@users.noreply.github.com>
2022-12-07 22:11:22 +0100

    Update Access.java

--- reminder/src/main/java/hikma/reminder/api/Access.java
+++ reminder/src/main/java/hikma/reminder/api/Access.java
@@ -1,20 +1,26 @@
 package hikma.reminder.api;
 
-import kong.unirest.Unirest;
-import hikma.reminder.api.Configuration;
-
+import kong.unirest.*;
+import kong.unirest.json.JSONObject;
 public class Access {
-    public void getTimingsByAddress(String address){
-        Unirest.get("%s%s%s".formatted(
+    public static JSONObject getJson(HttpRequest httpRequest){
+        return new JSONObject(httpRequest.asJson().getBody().toString());
+    }
+    public static JSONObject getTimingsByAddress(String address){
+        return getJson(
+                Unirest.get("%s%s%s".formatted(
                 Configuration.baseUrl,
                 Configuration.byAddress,
-                Configuration.addQueryParameter("address", address,true)));
+                Configuration.addQueryParameter("address", address,true))))
+                .getJSONObject(Configuration.JSON_DATA_KEY);
     }
-    public void getTimingsByCity(String country, String city){
-        Unirest.get("%s%s%s%s".formatted(
+    public static JSONObject getTimingsByCity(String country, String city){
+        return getJson(
+                Unirest.get("%s%s%s%s".formatted(
                 Configuration.baseUrl,
                 Configuration.byAddress,
                 Configuration.addQueryParameter("address", city,true),
-                Configuration.addQueryParameter("address", country,false)));
+                Configuration.addQueryParameter("address", country,false))))
+                .getJSONObject(Configuration.JSON_DATA_KEY);
     }
 }
