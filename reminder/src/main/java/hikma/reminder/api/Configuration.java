970ab8c148566bb55cecb69058e6f156ef764ca5
nkrumah <79527821+vsvincent@users.noreply.github.com>
2022-12-07 22:08:39 +0100

    Expand constants in Configuration

--- reminder/src/main/java/hikma/reminder/api/Configuration.java
+++ reminder/src/main/java/hikma/reminder/api/Configuration.java
@@ -4,6 +4,13 @@ public class Configuration {
     static final String baseUrl = "http://api.aladhan.com/v1/";
     static final String byAddress = "timingsByAddress";
     static final String byCity = "timingsByCity";
+    public static final String JSON_DATA_KEY = "data";
+    public static final String JSON_TIMING_KEY = "timings";
+    public static final String JSON_DATE_KEY = "date";
+    public static final String JSON_GREGORIAN_KEY = "gregorian";
+    public static final String JSON_META_KEY = "meta";
+    public static final String JSON_TIMEZONE_KEY = "timezone";
+    public static final String DATE_FORMAT = "dd-MM-yyyy";
     public static String addQueryParameter(String parameterName, String parameterValue, Boolean firstParameter){
         if(firstParameter){
             return "?" + parameterName + "=" + parameterValue;
